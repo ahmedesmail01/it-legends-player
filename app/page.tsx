d51be85df3ac093874d5562484749edb59e811b9
Ahmed Esmail <ahmedp30895533@pharm.tanta.edu.eg>
2025-10-14 17:59:48 +0300

    chore: update dependencies in package.json
    - Added @ant-design/icons version 5.x
    - Added antd version ^5.27.5
    - Updated next to version 15.5.5

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
+import { FacebookOutlined } from "@ant-design/icons";
+import { DatePicker } from "antd";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <DatePicker />
+        <div className="bgwhite p-4 rounded shadow">
+          <FacebookOutlined />
+        </div>
+
         <Image
           className="dark:invert"
           src="/next.svg"
